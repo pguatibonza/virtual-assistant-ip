4addab38248630025279e0600825144b6f4a0970
Pablito <p.guatibonza@uniandes.edu.co>
2024-09-09 21:20:58 -0500

    estructura completa

--- backend/model.py
+++ backend/model.py
@@ -19,10 +19,11 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import MemorySaver
 from pprint import pprint
-from typing import Any,  Literal, Union
+from typing import Any,  Literal, Union, Optional,Callable
 from langchain_core.messages import  AnyMessage
 from langchain.schema import AIMessage
 from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.messages import ToolMessage
 import logging
 import os
 import load_data
@@ -35,35 +36,44 @@ AZURE_OPENAI_ENDPOINT=os.getenv("AZURE_OPENAI_ENDPOINT")
 OPENAI_API_VERSION=os.getenv("OPENAI_API_VERSION")
 
 vector_store=load_data.load_vector_store()
-retriever=vector_store.as_retriever(search_kwargs={"k":4})
+retriever=vector_store.as_retriever(search_kwargs={"k":1})
 chat= AzureChatOpenAI(azure_deployment="gpt-4o-rfmanrique")
 
 
+class CompleteOrEscalate(BaseModel):
+    """A tool to mark the current task as completed and/or to escalate control of the dialog to the main assistant,
+    who can re-route the dialog based on the student's needs."""
+
+    cancel: bool =True
+    reason: str
 
 #Prompt feedback 
 system = """
 
-"Eres un tutor de programación encargado de brindar retroalimentación formativa a estudiantes del curso de introducción a la programación, quienes estan aprendiendo python. 
-Tu vas a recibir toda la información del problema que se quiere revisar. 
+You are a specialized teaching assistant in the introduction to programming course.
+The primary assistant delegates work to you whenever the user needs help in this programming code submission
+Your main task is to give formative feedback to the students of the introduction to programming course, in python.
+You are going to receive all the problem info to attend.
+If the student changes their mind, escalate the task back to the main assistant.
+If the student needs help and your function is not appropiate to answer him, then CompleteOrEscalate.
+If the student input is about conceptual information about programming, or any requests not about coding assistant, you must CompleteOrEscalate
 
-No escribas bloques de código de ejemplo. No escribas una versión corregida o actualizada del código del estudiante. 
-No debes escribir código para el estudiante. ¿Cómo responderías al estudiante para guiarlo y explicar conceptos sin proporcionar un ejemplo de código?"
 
-
-La información consiste en :
-1. Descripción del problema :
+Dont write any lines of code. Dont write a correct or updated version of the students code.
+You must not write code for the student. How would you answer to guide the student and explain concepts to him without writing a code example.
+The information consists of  :
+1. Problem description :
 {problem_description}
-2. Parametros de entrada:
+2. Input parameters:
 {parameter_description} 
-3. Retorno 
+3. Return
 {return_description}
-4. Funciones y primitivas prohibidas :
+4. Function and primitives forbidden :
 {primitives_forbiden_description}
 
-La solución del estudiante no puede contener ninguna de las funciones y primitivas prohibidas. 
+The student solution cannot contain any of the function or primitives forbidden 
 
-Los estudiantes proporcionarán el codigo de la solución : 
-{user_input}
+Student code solution : {user_input}
 
 Si el user_input del usuario no es codigo, responde con un error diciendole que debe proveer el codigo del solución que tiene planeada. Debes ser simple y directo
 
@@ -71,26 +81,22 @@ Si el user_input es codigo, entonces:
 Analiza el codigo proporcionado por el usuario, y responde con una explicacion educativa, ayudando al estudiante a descubrir los problemas con su solución. 
 Si la solución no presenta errores, dile al ususario que lo hizo muy bien. Se simple y conciso con tu respuesta
 
+Make sure that the feedback is constructive and easy to understand, avoiding any kind of negative reviews and focusing in how the student can learn and improve
 
-Asegurate de que la retroalimentación sea constructiva y fácil de entender, evitando cualquier tipo de critica negativa y enfocandote en como el estudiante puede aprender y mejorar
+Be positive. Use the markdown format, including the ‘ for online coding.
+Make your own solution first to look any differences with the student solution
 
-¡Sé positivo y alentador! Utiliza el formato Markdown, incluyendo ‘ para código en línea.
-Primero realiza tu la solución para mirar diferencias con la solución que te manda el estudiante.
-
-Recuerda que no puedes escribir bloques de codigo.
-
-
+Remember you cannot write any lines of code
 """
 
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system),
-        
         ("placeholder","{messages}")
     ]
 )
 
-feedback_agent=prompt | chat
+feedback_agent=prompt | chat.bind_tools([CompleteOrEscalate])
 
 descripcion="""
 
@@ -148,29 +154,29 @@ chat= AzureChatOpenAI(azure_deployment="gpt-4o-rfmanrique")
 
 
 system="""
-Eres un monitor asistente de la clase de introducción a la programación en Python. Tu función principal es responder dudas conceptuales que los estudiantes tengan sobre los diferentes módulos del curso. El curso está dividido en 4 módulos:
+You are a specialized assistant for Answering conceptual doubts about the "introduction to programming course" 
+The main assistant delegates work to you whenever the student needs conceptual help.
+If the student changes their mind, escalate the task back to the main assistant.
+Your main function is to answer conceptual doubts/inquiries that students may have about the different modules of the course. The course is compound of 4 modules:
 
-Módulo 1: Introducción a la programación
+Module 1 :  Introduction to programming
+In this module, students learn about the fundaments of programming.
+Module 2 : Conditions
+Students learn conditional structures like if, elif and else.Boolean algebra
+Module 3 : Loops
+Students learn about for and while 
+Module 4 : Pandas
+Students learn to use external libraries, like pandas.
 
-En este módulo, los estudiantes aprenden los fundamentos de la programación, cómo funcionan los lenguajes de programación y el proceso de escribir, ejecutar y depurar código.
-Módulo 2: Condicionales
-
-Aquí los estudiantes estudian las estructuras condicionales como if, else, y elif para tomar decisiones en el código.
-Módulo 3: Bucles
-
-En este módulo se cubren bucles for y while, junto con conceptos como control de bucles (break, continue).
-Módulo 4: Librerías
-
-Los estudiantes exploran cómo importar y utilizar librerías en Python, como math o random, y cómo instalar librerías externas.
-Para responder preguntas de los estudiantes, tendrás como referencia el siguiente contexto tomado de un libro de programación:
+To answer the student doubts, you will have the following context taken from a programming book :
 
 {context}
 
-Tu tarea es usar este contexto para responder de manera precisa, clara y útil a las preguntas. Intenta explicar los conceptos con ejemplos y lenguaje accesible para principiantes, proporcionando respuestas bien estructuradas y fáciles de entender. Si es necesario, usa fragmentos de código simples para ilustrar los conceptos.
-El usuario se encuentra en el nivel : {level}
-Utiliza el formato Markdown, incluyendo ‘ para código en línea.
+Try to explain the concepts in the most briefly way. If the student wants to emphasize in a particular item, proceed. 
+The student is on the level : {level}
 
-Input del usuario : {user_input}
+Use markdown format, including ‘ for online coding
+Student input : {user_input}
 """
 
 prompt = ChatPromptTemplate.from_messages(
@@ -180,7 +186,7 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-conceptual_agent=prompt|chat
+conceptual_agent=prompt|chat.bind_tools([CompleteOrEscalate])
 
 #Prompt  Question re-writer
 
@@ -203,14 +209,93 @@ chat= AzureChatOpenAI(azure_deployment="gpt-4o-rfmanrique")
 
 question_rewriter = re_write_prompt | chat  
 
+#Main assistant 
+chat= AzureChatOpenAI(azure_deployment="gpt-4o-rfmanrique")
+class toConceptualAssistant(BaseModel):
+    """
+    Transfers work to a specialized assistant  to handle any conceptual doubts/inquiries about the programming course. The doubts must be about programming
+    """
+    request: str=Field(description="Any necessary follow-up questions the conceptual assistant  should clarify  before proceeding. The questions must be about programming. ")
+
+class toFeedbackAssistant(BaseModel):
+    """
+    Transfers work to a specialized assistant to give detailed feedback about a code problem
+    """
+    code : str=Field(description="The code block which the student wants to get feedback from")
+    name : str=Field(description= "The name of the coding problem which the student wants to get feedback from")
+
+system = """
+You are a helpful teaching assistant for the introducion to programming class. 
+Your primary role is to welcome the student and introduce them your main capabilities.
+Your main capabilities are : Give feedback to students about a code submission of a problem in Senecode, the course main platform;
+and Assist the user with any conceptual doubts he has about the programming course. 
+The conceptual doubts the student may have are from the topics :  Variables, operator, conditionals, boolean algebra, loops, external libraries
+Only the specialized assistants are given permission  to do this for the student.
+The student is not aware of the different specialized assistants, so do not mention them; just quietly delegate through function calls. 
+Only do functions calls when the user input is not enough to know which tool to use
+Be simple and concise with your conversations.
+if the student input is not related to programming or your capabilities, dont do function calls. Talk in spanish
+"""
+
+primary_assistant_prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system",system),
+        ("placeholder","{messages}"),
+        ("human", "Student input : {user_input}" ),
+        
+    ]
+)
+
+main_assistant = primary_assistant_prompt | chat.bind_tools([toConceptualAssistant,toFeedbackAssistant])
+
+#Utilities
+
+
+def create_entry_node(assistant_name: str, new_dialog_state: str) -> Callable:
+    def entry_node(state: State) -> dict:
+        tool_call_id = state["messages"][-1].tool_calls[0]["id"]
+        return {
+            "messages": [
+                ToolMessage(
+                    content=f"The assistant is now the {assistant_name}. Reflect on the above conversation between the host assistant and the user."
+                    f" The user's intent is unsatisfied. Use the provided tools to assist the user. Remember, you are {assistant_name},"
+                    " If the user changes their mind or needs help for other tasks, call the CompleteOrEscalate function to let the primary host assistant take control."
+                    " Do not mention who you are - just act as the proxy for the assistant.",
+                    tool_call_id=tool_call_id,
+                )
+            ],
+            "dialog_state": new_dialog_state,
+        }
+
+    return entry_node
+#Graph
+def update_dialog_stack(left: list[str], right: Optional[str]) -> list[str]:
+    """Push or pop the state."""
+    if right is None:
+        return left
+    if right == "pop":
+        return left[:-1]
+    return left + [right]
 
 
 class State(TypedDict):
     messages:Annotated[list,add_messages]
+    dialog_state : Annotated[
+        list[
+            Literal[
+                "main_assistant",
+                "feedback_assistant",
+                "conceptual_assistant"
+            ]
+        ],update_dialog_stack
+    ]
+    level : str
 
 graph_builder=StateGraph(State)
 #memory = SqliteSaver.from_conn_string(":memory:")
 memory = MemorySaver()
+
+
 def senecode_assistant(state:State):
     message=feedback_agent.invoke(
     {"problem_description":descripcion,"parameter_description":parametros,
@@ -218,25 +303,118 @@ def senecode_assistant(state:State):
 
     return {"messages": [message]}
 def conceptual_assistant(state:State):
-    user_input=state['messages'][-1]
+    user_input=state['messages'][-2].tool_calls[0]['args']['request']
     #Reformula pregunta para vector store
-    query=question_rewriter.invoke({"user_input": user_input, "messages":state["messages"]}).content
+    #query=question_rewriter.invoke({"user_input": user_input, "messages":state["messages"]}).content
 
     #Extrae contexto segun el query
-    context=retriever.invoke(query)
+    context=retriever.invoke(user_input)
 
     #Responde de acuerdo al contexto
     response=conceptual_agent.invoke({"user_input":user_input,"messages":state["messages"],"context":context,"level":state["level"]})
     return {"messages": [response]}
 
+def primary_assistant(state:State):
+    user_input=state['messages'][-1]
+    message=main_assistant.invoke({"user_input":user_input})
 
-graph_builder.add_node("senecode_assistant",senecode_assistant)
-graph_builder.add_edge(START, "senecode_assistant")
-graph_builder.add_edge("senecode_assistant", END)
+    return {"messages":[message],"user_input": user_input}
+
+def route_primary_assistant(
+    state: State,
+) -> Literal[
+    "enter_conceptual_assistant",
+    "enter_feedback_assistant",
+    "__end__",
+]:
+    route = tools_condition(state)
+    if route == END:
+        return END
+    tool_calls = state["messages"][-1].tool_calls
+    if tool_calls:
+        if tool_calls[0]["name"] == toConceptualAssistant.__name__:
+            return "enter_conceptual_assistant"
+        elif tool_calls[0]["name"] == toFeedbackAssistant.__name__:
+            return "enter_feedback_assistant"
+    raise ValueError("Invalid route")
+
+def route_to_workflow(
+    state: State,
+) -> Literal[
+    "primary_assistant",
+    "conceptual_assistant",
+    "feedback_assistant",
+]:
+    """If we are in a delegated state, route directly to the appropriate assistant."""
+    dialog_state = state.get("dialog_state")
+    if not dialog_state:
+        return "primary_assistant"
+    return dialog_state[-1]
+#Por el momento se usara uno en conjunto apra feedback y conceptual
+def route_assistants(
+    state: State,
+) -> Literal[
+    "leave_skill",
+    "__end__",
+]:
+    route = tools_condition(state)
+    if route == END:
+        return END
+    tool_calls = state["messages"][-1].tool_calls
+    did_cancel = any(tc["name"] == CompleteOrEscalate.__name__ for tc in tool_calls)
+    if did_cancel:
+        return "leave_skill"
+
+# This node will be shared for exiting all specialized assistants
+def pop_dialog_state(state: State) -> dict:
+    """Pop the dialog stack and return to the main assistant.
+
+    This lets the full graph explicitly track the dialog flow and delegate control
+    to specific sub-graphs.
+    """
+    messages = []
+    if state["messages"][-1].tool_calls:
+        # Note: Doesn't currently handle the edge case where the llm performs parallel tool calls
+        messages.append(
+            ToolMessage(
+                content="Resuming dialog with the host assistant. Please reflect on the past conversation and assist the student as needed.",
+                tool_call_id=state["messages"][-1].tool_calls[0]["id"],
+            )
+        )
+    return {
+        "dialog_state": "pop",
+        "messages": messages,
+    }
+
+
+graph_builder.add_conditional_edges(START,route_to_workflow)
+graph_builder.add_node("primary_assistant",primary_assistant)
+
+graph_builder.add_node("enter_conceptual_assistant",create_entry_node("Conceptual Programming assistant","conceptual_assistant"))
+graph_builder.add_node("conceptual_assistant",conceptual_assistant)
+graph_builder.add_edge("enter_conceptual_assistant", "conceptual_assistant")
+
+graph_builder.add_node("enter_feedback_assistant",create_entry_node("Feedback assistant","feedback_assistant"))
+graph_builder.add_node("feedback_assistant",senecode_assistant)
+graph_builder.add_edge("enter_feedback_assistant", "feedback_assistant")
+
+graph_builder.add_conditional_edges(
+    "primary_assistant",
+    route_primary_assistant,
+    {
+        "enter_conceptual_assistant": "enter_conceptual_assistant",
+        "enter_feedback_assistant": "enter_feedback_assistant",
+        END: END,
+    },
+)
+graph_builder.add_conditional_edges("conceptual_assistant", route_assistants)
+graph_builder.add_conditional_edges("feedback_assistant",route_assistants)
+graph_builder.add_node("leave_skill",pop_dialog_state)
+graph_builder.add_edge("leave_skill", "primary_assistant")
 
 graph = graph_builder.compile(checkpointer=memory)
-config={"configurable":{"thread_id":1}}
-lista=["hola",solucion1,solucion2,"como puedes mejorar el codigo"]
+config={"configurable":{"thread_id":12}}
+lista=["ahora quiero que me ayudes a corregir el codigo de un problema"]
 for i in lista:
     for event in graph.stream({"messages": ("user", i),"level":2},config):
         for value in event.values():
